a112d62143c115741f812d91935bdf930878c024
antiengineer <noordhuisd@gmail.com>
2020-01-21 03:43:18 -0500

    Create ghetto TenantManager

---
+++ BlackHawk/classes/managers/tenants/TenantManager.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: TenantManager.php
+ *
+ *
+ * Created: 1/21/20, 3:43 AM
+ * Last modified: 1/21/20, 2:27 AM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\classes\managers\tenants;
+
+
+class TenantManager
+{
+
+}
