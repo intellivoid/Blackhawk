c4b7d3daad989f0b658e15599ad2815159c9beb7
antiengineer <noordhuisd@gmail.com>
2020-01-21 03:41:36 -0500

    Add Config Exceptions

---
+++ BlackHawk/exceptions/configuration/ConfigParseException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: ConfigParseException.php
+ *
+ *
+ * Created: 1/21/20, 3:41 AM
+ * Last modified: 1/20/20, 1:29 PM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\exceptions\configuration;
+
+class ConfigParseException extends \RuntimeException
+{
+
+}
