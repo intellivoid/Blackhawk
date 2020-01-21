0112fdb500d384489ceab46425930cd139705a51
antiengineer <noordhuisd@gmail.com>
2020-01-21 03:43:40 -0500

    Create core entrypoint for BlackHawk

--- BlackHawk/BlackHawk.php
+++ BlackHawk/BlackHawk.php
@@ -5,8 +5,8 @@
  * BlackHawk: BlackHawk.php
  *
  *
- * Created: 1/20/20, 1:04 PM
- * Last modified: 1/19/20, 9:25 PM
+ * Created: 1/21/20, 3:43 AM
+ * Last modified: 1/21/20, 2:27 AM
  * Modified by: intellivoid/antiengineer
  *
  * @copyright 2020 (C) Nighthawk Media Group
@@ -19,8 +19,30 @@
 
 namespace BlackHawk;
 
+use BlackHawk\classes\managers\security\CryptoManager;
+use BlackHawk\classes\managers\tenants\TenantManager;
+use BlackHawk\objects\Configuration;
 
+/**
+ * Class BlackHawk
+ * @package BlackHawk
+ */
 class BlackHawk
 {
+    /**
+     * @var Configuration
+     */
+    private $_configObj;
+
+    /**
+     * @var TenantManager
+     */
+    private $_tenantManager;
+
+    /**
+     * @var CryptoManager
+     */
+    private $_cryptoManager;
+
 
 }
