e92c5bfe75a2cf8cfa63619d1f2099b198debaca
antiengineer <noordhuisd@gmail.com>
2020-01-22 05:25:57 -0500

    Create Runtime Exceptions

---
+++ BlackHawk/exceptions/runtime/RequestException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: RequestException.php
+ *
+ *
+ * Created: 1/22/20, 5:25 AM
+ * Last modified: 1/21/20, 6:47 AM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\exceptions\runtime;
+
+
+class RequestException extends \RuntimeException
+{
+
+}
---
+++ BlackHawk/exceptions/runtime/ServerErrorException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: ServerErrorException.php
+ *
+ *
+ * Created: 1/22/20, 5:25 AM
+ * Last modified: 1/21/20, 6:48 AM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\exceptions\runtime;
+
+
+class ServerErrorException extends \RuntimeException
+{
+
+}
