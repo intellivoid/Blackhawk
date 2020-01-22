43a4543896642c381526e2f0305e121992272306
antiengineer <noordhuisd@gmail.com>
2020-01-22 16:49:33 -0500

    Add Routing Exceptions

---
+++ BlackHawk/exceptions/tenants/router/RouteExistsException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: RouteExistsException.php
+ *
+ *
+ * Created: 1/22/20, 4:49 PM
+ * Last modified: 1/22/20, 4:24 PM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\exceptions\tenants\router;
+
+
+class RouteExistsException extends \RuntimeException
+{
+
+}
---
+++ BlackHawk/exceptions/tenants/router/RouteNotFoundException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: RouteNotFoundException.php
+ *
+ *
+ * Created: 1/22/20, 4:49 PM
+ * Last modified: 1/22/20, 4:31 PM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\exceptions\tenants\router;
+
+
+class RouteNotFoundException extends \RuntimeException
+{
+
+}
