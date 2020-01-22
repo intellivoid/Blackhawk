08fd0efbe7b024f061d627aa2c8e6e12cb08415c
antiengineer <noordhuisd@gmail.com>
2020-01-22 16:58:55 -0500

    Added fields and partial logic on Main class

--- BlackHawk/BlackHawk.php
+++ BlackHawk/BlackHawk.php
@@ -5,8 +5,8 @@
  * BlackHawk: BlackHawk.php
  *
  *
- * Created: 1/21/20, 3:43 AM
- * Last modified: 1/21/20, 2:27 AM
+ * Created: 1/22/20, 4:58 PM
+ * Last modified: 1/22/20, 12:12 PM
  * Modified by: intellivoid/antiengineer
  *
  * @copyright 2020 (C) Nighthawk Media Group
@@ -22,6 +22,7 @@ namespace BlackHawk;
 use BlackHawk\classes\managers\security\CryptoManager;
 use BlackHawk\classes\managers\tenants\TenantManager;
 use BlackHawk\objects\Configuration;
+use BlackHawk\objects\SecureStorage;
 
 /**
  * Class BlackHawk
@@ -44,5 +45,43 @@ class BlackHawk
      */
     private $_cryptoManager;
 
+    /**
+     * @var SecureStorage
+     */
+    private $_secureStorage;
 
+    /**
+     * @return Configuration
+     */
+    public function getConfig(): Configuration
+    {
+        return $this->_configObj;
+    }
+
+    /**
+     * @return TenantManager
+     */
+    public function getTenantManager(): TenantManager
+    {
+        return $this->_tenantManager;
+    }
+
+
+    /**
+     * @return SecureStorage
+     */
+    public function getSecureStorage(): SecureStorage
+    {
+        return $this->_secureStorage;
+    }
+
+    /**
+     * BlackHawk constructor.
+     */
+    public function __construct(){
+        $this->_configObj = new Configuration();
+        $this->_tenantManager = new TenantManager();
+        $this->_cryptoManager = new CryptoManager();
+        $this->_secureStorage = new SecureStorage($this);
+    }
 }
