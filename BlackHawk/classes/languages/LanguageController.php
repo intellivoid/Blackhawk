b16a85e114dfe88de6db8596d555dd4c0bffbea7
antiengineer <noordhuisd@gmail.com>
2020-02-01 12:23:16 -0500

    Add Language Controller

---
+++ BlackHawk/classes/languages/LanguageController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BlackHawk Engine
+ *
+ * BlackHawk: LanguageController.php
+ *
+ *
+ * Created: 2/1/20, 12:23 PM
+ * Last modified: 1/31/20, 4:13 PM
+ * Modified by: intellivoid/antiengineer
+ *
+ * @copyright 2020 (C) Nighthawk Media Group
+ * @author Diederik Noordhuis, Zi Xing Narrakas
+ *
+ * For more information, contact diederikn@intellivoid.info.
+ * No modifications allowed. Distribution is prohibited.
+ *
+ */
+
+namespace BlackHawk\classes\languages;
+
+
+class LanguageController
+{
+
+}
